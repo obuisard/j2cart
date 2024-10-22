5c7d3ea84b526d2118dc422cb152ad5b15fd5d11
Adam Melcher <adam@newlinewebdesign.com>
2024-10-21 19:58:20 -0500

    Update Mini-Stats Admin Module
    Update Mini-Stats admin module to Bootstrap 5 / Joomla standards

--- modules/admin/j2store_stats_mini/tmpl/default.php
+++ modules/admin/j2store_stats_mini/tmpl/default.php
@@ -6,6 +6,10 @@
  */
 // No direct access to this file
 defined ( '_JEXEC' ) or die ();
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+
 require_once (JPATH_ADMINISTRATOR.'/components/com_j2store/helpers/j2store.php');
 $currency = J2Store::currency();
 $order_status = $params->get('order_status', array('*'));
@@ -16,117 +20,129 @@ if (version_compare(JVERSION, '3.99.99', 'lt')) {
     $col_class = 'span';
 }
 ?>
-<div class="j2store_stat-mini">
-    <div class="<?php echo $row_class; ?>">
+<div class="card mb-5 mt-3">
+    <div class="card-body">
+        <nav class="quick-icons" aria-label="J2Commerce Mini Stats Notifications">
+            <div class="row flex-wrap">
+                <div class="quickicon quickicon-single col border-0">
+	                <?php
+	                $tz = Factory::getApplication()->getConfig()->get('offset');
+	                $today = Factory::getDate('now', $tz)->format('Y-m-d');
+	                $tommorow = Factory::getDate('now +1 days', $tz)->format('Y-m-d');
+	                ?>
+                    <div class="alert alert-success my-0 w-100 border-0">
+                        <div class="quickicon-info">
+                            <div class="quickicon-value display-6 mb-3">
 
-        <div class="<?php echo $col_class; ?>3">
-            <div class="j2store-stats-mini-badge j2store-stats-mini-today">
-                <?php
-                $tz = JFactory::getConfig()->get('offset');
-                $today = JFactory::getDate('now', $tz)->format('Y-m-d');
-                $tomorrow = JFactory::getDate('now +1 days', $tz)->format('Y-m-d');
-                ?>
-                <span class="j2store-mini-price">
-			<?php
-            echo $currency->format(
-                F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
-                    ->since($today)
-                    ->until($tomorrow)
-                    ->orderstatus($order_status)
-                    ->nozero(1)
-                    ->moneysum(1)
-                    ->getOrdersTotal()
-            );
-            ?>
-			</span>
-                <h3><?php echo JText::_('J2STORE_TOTAL_CONFIRMED_ORDERS_TODAY'); ?></h3>
+	                            <?php echo $currency->format(
+		                            F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
+			                            ->since($today)
+			                            ->until($tommorow)
+			                            ->orderstatus($order_status)
+			                            ->nozero(1)
+			                            ->moneysum(1)
+			                            ->getOrdersTotal()
+	                            );
+	                            ?>
+                            </div>
+                        </div>
+                        <div class="quickicon-name d-flex align-items-center">
+                            <span class="j-links-link">
+                                <div class="text-capitalize"><?php echo Text::_('J2STORE_TOTAL_CONFIRMED_ORDERS_TODAY'); ?></div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="quickicon quickicon-single col border-0">
+                    <div class="alert alert-warning my-0 w-100 border-0">
+                        <div class="quickicon-info">
+                            <div class="quickicon-value display-6 mb-3">
+	                            <?php
+	                            $yesterday = Factory::getDate('now -1 days', $tz)->format('Y-m-d');
+	                            echo $currency->format(
+		                            F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
+			                            ->since($yesterday)
+			                            ->until($yesterday . ' 23:59:59')
+			                            ->orderstatus($order_status)
+			                            ->nozero(1)
+			                            ->moneysum(1)
+			                            ->getOrdersTotal()
+	                            );
+	                            ?>
+                            </div>
+                        </div>
+                        <div class="quickicon-name d-flex align-items-center">
+                            <span class="j-links-link">
+                                <div class="text-capitalize"><?php echo Text::_('J2STORE_TOTAL_CONFIRMED_ORDERS_YESTERDAY'); ?></div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="quickicon quickicon-single col border-0">
+                    <div class="alert alert-primary my-0 w-100 border-0">
+                        <div class="quickicon-info">
+                            <div class="quickicon-value display-6 mb-3">
+	                            <?php
+	                            switch (gmdate('m')) {
+		                            case 1:
+		                            case 3:
+		                            case 5:
+		                            case 7:
+		                            case 8:
+		                            case 10:
+		                            case 12:
+			                            $lmday = 31;
+			                            break;
+		                            case 4:
+		                            case 6:
+		                            case 9:
+		                            case 11:
+			                            $lmday = 30;
+			                            break;
+		                            case 2:
+			                            $y = gmdate('Y');
+			                            if (!($y % 4) && ($y % 400)) {
+				                            $lmday = 29;
+			                            } else {
+				                            $lmday = 28;
+			                            }
+	                            }
+	                            if ($lmday < 1) $lmday = 28;
+	                            ?>
+	                            <?php echo $currency->format(
+		                            F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
+			                            ->since(gmdate('Y') . '-' . gmdate('m') . '-01')
+			                            ->until(gmdate('Y') . '-' . gmdate('m') . '-' . $lmday . ' 23:59:59')
+			                            ->orderstatus($order_status)
+			                            ->nozero(1)
+			                            ->moneysum(1)
+			                            ->getOrdersTotal()
+	                            );
+	                            ?>
+                            </div>
+                        </div>
+                        <div class="quickicon-name d-flex align-items-center">
+                            <span class="j-links-link">
+                                <div class="text-capitalize"><?php echo Text::_('J2STORE_TOTAL_CONFIRMED_ORDERS_THIS_MONTH'); ?></div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="quickicon quickicon-single col border-0">
+                    <div class="alert alert-danger my-0 w-100 border-0">
+                        <div class="quickicon-info">
+                            <div class="quickicon-value display-6 mb-3">
+	                            <?php echo $currency->format(F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()->orderstatus($order_status)->nozero(1)->moneysum(1)->getOrdersTotal()); ?>
+                            </div>
+                        </div>
+                        <div class="quickicon-name d-flex align-items-center">
+                            <span class="j-links-link">
+                                <div class="text-capitalize"><?php echo Text::_('J2STORE_ALL_TIME'); ?></div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div class="<?php echo $col_class; ?>3">
-            <div class="j2store-stats-mini-badge j2store-stats-mini-yesterday">
-                <?php
-                $yesterday = JFactory::getDate('now -1 days', $tz)->format('Y-m-d');
-                ?>
-                <span class="j2store-mini-price">
-			<?php
-            echo $currency->format(
-                F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
-                    ->since($yesterday)
-                    ->until($yesterday . ' 23:59:59')
-                    ->orderstatus($order_status)
-                    ->nozero(1)
-                    ->moneysum(1)
-                    ->getOrdersTotal()
-            );
-            ?>
-			</span>
-                <h3><?php echo JText::_('J2STORE_TOTAL_CONFIRMED_ORDERS_YESTERDAY'); ?></h3>
-            </div>
-        </div>
-
-        <div class="<?php echo $col_class; ?>3">
-            <div class="j2store-stats-mini-badge j2store-stats-mini-this-month">
-                <?php
-                switch (gmdate('m')) {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
-                        $lmday = 31;
-                        break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        $lmday = 30;
-                        break;
-                    case 2:
-                        $y = gmdate('Y');
-                        if (!($y % 4) && ($y % 400)) {
-                            $lmday = 29;
-                        } else {
-                            $lmday = 28;
-                        }
-                }
-                if ($lmday < 1) $lmday = 28;
-                ?>
-                <?php
-                /* 	echo F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
-                                        ->since(gmdate('Y').'-'.gmdate('m').'-01')
-                                        ->until(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
-                                        ->orderstatus($order_status)
-                                        ->nozero(1)
-                                        ->getOrdersTotal(); */
-                ?>
-                <span class="j2store-mini-price">
-			<?php
-            echo $currency->format(
-                F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()
-                    ->since(gmdate('Y') . '-' . gmdate('m') . '-01')
-                    ->until(gmdate('Y') . '-' . gmdate('m') . '-' . $lmday . ' 23:59:59')
-                    ->orderstatus($order_status)
-                    ->nozero(1)
-                    ->moneysum(1)
-                    ->getOrdersTotal()
-            );
-            ?>
-			</span>
-                <h3><?php echo JText::_('J2STORE_TOTAL_CONFIRMED_ORDERS_THIS_MONTH'); ?></h3>
-            </div>
-        </div>
-
-        <div class="<?php echo $col_class; ?>3">
-            <div class="j2store-stats-mini-badge j2store-stats-mini-orders">
-				<span class="j2store-mini-price">
-					<?php echo $currency->format(F0FModel::getTmpInstance('Orders', 'J2StoreModel')->clearState()->orderstatus($order_status)->nozero(1)->moneysum(1)->getOrdersTotal()); ?>
-				</span>
-                <h3><?php echo JText::_('J2STORE_ALL_TIME'); ?></h3>
-            </div>
-        </div>
-
+        </nav>
     </div>
 </div>
