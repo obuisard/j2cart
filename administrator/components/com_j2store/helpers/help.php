75055ec857ad30f6e798aba73257118235081f0e
J2Commerce <olivier@j2commerce.com>
2024-10-22 19:33:34 -0400

    aria-hidden missing
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- administrator/components/com_j2store/helpers/help.php
+++ administrator/components/com_j2store/helpers/help.php
@@ -38,7 +38,7 @@ class J2Help {
 						 			</p>
 						 		</div>';
     $newhtml = '<div class="alert alert-info d-flex align-items-center" role="alert">
-            <i class="fas fa-solid fa-info-circle flex-shrink-0 me-2"></i>
+            <span class="fas fa-solid fa-info-circle flex-shrink-0 me-2" aria-hidden="true"></span>
             <div>'.JText::_('J2STORE_TAKEOVER_INFO').'</div>
             <a href="https://www.j2commerce.com" class="btn btn-sm btn-dark text-light ms-3 ms-lg-auto" title="Visit J2Commerce" target="_blank"  rel="noreferrer noopener">'.JText::_('J2STORE_FIND_OUT_MORE').'</a>
         </div>';
