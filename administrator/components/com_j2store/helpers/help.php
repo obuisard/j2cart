4166a203fb6e725004f2d3fffc24ca4eac924181
Adam Melcher <54608117+newlinewebdesign@users.noreply.github.com>
2024-10-25 09:25:13 -0500

    Fix Joomla 4 FA icon + Joomla 5 name spacing (#37)

--- administrator/components/com_j2store/helpers/help.php
+++ administrator/components/com_j2store/helpers/help.php
@@ -2,10 +2,15 @@
 /**
  * @package J2Store
  * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
+ * @copyright Copyright (c) 2024 J2Commerce
  * @license GNU GPL v3 or later
  */
 // No direct access to this file
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Session\Session;
 
 /**
  * J2Store help texts and videos.
@@ -32,15 +37,15 @@ class J2Help {
   public function watch_video_tutorials() { // update later with j2commerce youtube channel
     $video_url = J2Store::buildHelpLink('support/video-tutorials.html', 'dashboard');
     $html = '<div class="video-tutorial panel panel-solid-info">
-				<p class="panel-body">'.JText::_('J2STORE_VIDEO_TUTORIALS_HELP_TEXT').'
+				<p class="panel-body">'.Text::_('J2STORE_VIDEO_TUTORIALS_HELP_TEXT').'
 						 				<a class="btn btn-success" target="_blank" href="'.$video_url.'">
-						 					'.JText::_('J2STORE_WATCH').'</a>
+						 					'.Text::_('J2STORE_WATCH').'</a>
 						 			</p>
 						 		</div>';
     $newhtml = '<div class="alert alert-info d-flex align-items-center" role="alert">
             <i class="fas fa-solid fa-info-circle flex-shrink-0 me-2"></i>
-            <div>'.JText::_('J2STORE_TAKEOVER_INFO').'</div>
-            <a href="https://www.j2commerce.com" class="btn btn-sm btn-dark text-light  ms-3 ms-lg-auto" title="'.JText::_('J2STORE_VISIT_J2COMMERCE').'" target="_blank"><i class="fas fa-solid fa-external-link me-2"></i>'.JText::_('J2STORE_FIND_OUT_MORE').'</a>
+            <div>'.Text::_('J2STORE_TAKEOVER_INFO').'</div>
+            <a href="https://www.j2commerce.com" class="btn btn-sm btn-dark text-light text-nowrap ms-3 ms-lg-auto" title="'.Text::_('J2STORE_VISIT_J2COMMERCE').'" target="_blank"><i class="fas fa-solid fa-external-link-alt fa-arrow-up-right-from-square me-2"></i>'.Text::_('J2STORE_FIND_OUT_MORE').'</a>
         </div>';
     return $newhtml;
   }
@@ -52,9 +57,9 @@ class J2Help {
 		}
 		$free_topbar_url = J2Store::buildHelpLink('/j2store-pro-features.html', 'dashboard');
 		$html = '<div class="video-tutorial free-topbar panel panel-solid-info">
-				<p class="panel-body">'.JText::_('J2STORE_FREE_TOPBAR_HELP_TEXT').'
+				<p class="panel-body">'.Text::_('J2STORE_FREE_TOPBAR_HELP_TEXT').'
 						 				<a class="btn btn-success" target="_blank" href="'.$free_topbar_url.'">
-						 					'.JText::_('J2STORE_UPGRADE_PRO').'</a>
+						 					'.Text::_('J2STORE_UPGRADE_PRO').'</a>
 						 			</p>
 						 		</div>';
 		return $html;
@@ -74,12 +79,12 @@ class J2Help {
             $class = 'alert alert-info';
         }
 		//message not hidden
-		$url = JRoute::_ ( 'index.php?option=com_j2store&view=cpanels&task=notifications&message_type=' . $type . '&' . JSession::getFormToken () . '=1' );
+		$url = Route::_ ( 'index.php?option=com_j2store&view=cpanels&task=notifications&message_type=' . $type . '&' . Session::getFormToken() . '=1' );
 		$html .= '<div class="user-notifications ' . $type . ' '.$class.'">';
 		$html .= '<h3>' . $title . '</h3>';
 		$html .= '<p>' . $message . '</p>';
 		$html .= '<br />';
-		$html .= '<a class="btn btn-danger" href="' . $url . '">' . JText::_ ( 'J2STORE_GOT_IT_AND_HIDE' ) . '</a>';
+		$html .= '<a class="btn btn-danger" href="' . $url . '">' . Text::_ ( 'J2STORE_GOT_IT_AND_HIDE' ) . '</a>';
 		$html .= '</div>';
 		return $html;
 	}
