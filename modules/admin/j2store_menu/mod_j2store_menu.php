e33de0ceef3fe73a5c991f00c10eaed9a225620c
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2024-11-04 16:14:50 -0500

    Added test on user to determine if the module should show or not

--- modules/admin/j2store_menu/mod_j2store_menu.php
+++ modules/admin/j2store_menu/mod_j2store_menu.php
@@ -1,21 +1,35 @@
 <?php
-/*------------------------------------------------------------------------
-# mod_j2store_menu
-# ------------------------------------------------------------------------
-# author    Gokila Priya - Weblogicx India http://www.weblogicxindia.com
-# copyright Copyright (C) 2014 - 19 Weblogicxindia.com. All Rights Reserved.
-# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
-# Websites: http://j2store.org
-# Technical Support:  Forum - http://j2store.org/forum/index.html
--------------------------------------------------------------------------*/
+/*
+ * mod_j2store_menu
+ */
+
+/**
+ * @copyright Copyright (C) 2014-2019 Weblogicx India. All rights reserved.
+ * @copyright Copyright (C) 2024 J2Commerce, Inc. All rights reserved.
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ * @website https://www.j2commerce.com
+ */
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
+
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
-if (!defined('F0F_INCLUDED'))
-{
-	include_once JPATH_LIBRARIES . '/f0f/include.php';
+defined('_JEXEC') or die('Restricted access');
+
+// The J2Store menu will not show if no user is logged in.
+$user = Factory::getApplication()->getIdentity();
+if ($user === null || $user->id === 0) {
+    return;
 }
-require_once( dirname(__FILE__).'/helper.php' );
-JFactory::getLanguage()->load('com_j2store', JPATH_ADMINISTRATOR);
-$moduleclass_sfx = $params->get('moduleclass_sfx','');
-$link_type = $params->get('link_type','link');
-require( JModuleHelper::getLayoutPath('mod_j2store_menu', $params->get('layout', 'default')));
+
+if (!defined('F0F_INCLUDED')) {
+    include_once JPATH_LIBRARIES . '/f0f/include.php';
+}
+
+require_once( dirname(__FILE__) . '/helper.php' );
+
+Factory::getLanguage()->load('com_j2store', JPATH_ADMINISTRATOR);
+$moduleclass_sfx = $params->get('moduleclass_sfx', '');
+$link_type = $params->get('link_type', 'link');
+
+require ModuleHelper::getLayoutPath('mod_j2store_menu', $params->get('layout', 'default'));
