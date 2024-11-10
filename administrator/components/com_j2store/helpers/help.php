3ffc2bdcd56b7c0b96984b39f08a27257ddd35ab
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2024-11-09 20:50:28 -0500

    Added info_j2commerce function + removed temporary code

--- administrator/components/com_j2store/helpers/help.php
+++ administrator/components/com_j2store/helpers/help.php
@@ -42,12 +42,7 @@ class J2Help {
 						 					'.Text::_('J2STORE_WATCH').'</a>
 						 			</p>
 						 		</div>';
-    $newhtml = '<div class="alert alert-info d-flex align-items-center" role="alert">
-            <i class="fas fa-solid fa-info-circle flex-shrink-0 me-2"></i>
-            <div>'.Text::_('J2STORE_TAKEOVER_INFO').'</div>
-            <a href="https://www.j2commerce.com" class="btn btn-sm btn-dark text-light text-nowrap ms-3 ms-lg-auto" title="'.Text::_('J2STORE_VISIT_J2COMMERCE').'" target="_blank"><i class="fas fa-solid fa-external-link-alt fa-arrow-up-right-from-square me-2"></i>'.Text::_('J2STORE_FIND_OUT_MORE').'</a>
-        </div>';
-    return $newhtml;
+    return $html;
   }
 
 	public function free_topbar() {
@@ -65,6 +60,37 @@ class J2Help {
 		return $html;
 	}
 
+	public function info_j2commerce()
+	{
+	    $html = '';
+
+	    $type = 'j2commerce_isnew';
+
+	    //check if this alert to be shown.
+	    $params = J2Store::config();
+	    if ($params->get($type, 0)) {
+	        return $html;
+	    }
+
+	    if (version_compare(JVERSION,'3.99.99','lt')) {
+	        $class = 'alert alert-block';
+	    } else {
+	        $class = 'alert alert-info';
+	    }
+
+	    $url = Route::_ ('index.php?option=com_j2store&view=cpanels&task=notifications&message_type=' . $type . '&' . Session::getFormToken() . '=1');
+
+	    $html .= '<div class="user-notifications ' . $type . ' ' . $class . '" role="alert">';
+	    $html .= '<p>';
+	    $html .= '<span class="fas fa-solid fa-info-circle flex-shrink-0 me-2"></span>';
+	    $html .= Text::_('J2STORE_TAKEOVER_INFO');
+	    $html .= '</p>';
+	    $html .= '<a class="btn btn-sm btn-dark text-light text-nowrap me-3" href="' . $url . '">' . Text::_('J2STORE_GOT_IT_AND_HIDE') . '</a>';
+	    $html .= '<a href="https://www.j2commerce.com" class="btn btn-sm btn-primary text-light text-nowrap me-3" title="'.Text::_('J2STORE_VISIT_J2COMMERCE').'" target="_blank"><span class="fas fa-solid fa-external-link-alt fa-arrow-up-right-from-square me-2"></span>'.Text::_('J2STORE_FIND_OUT_MORE').'</a>';
+	    $html .= '</div>';
+
+	    return $html;
+	}
 
 	public function alert($type, $title, $message) {
 
