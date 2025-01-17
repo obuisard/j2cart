4b27bf230107179459110700b6b592f83cac5b0d
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-01-16 22:25:03 -0500

    Changed copyright year

--- plugins/system/j2store/j2store.php
+++ plugins/system/j2store/j2store.php
@@ -4,13 +4,13 @@
  * @subpackage  System.j2store
  *
  * @copyright Copyright (C) 2014-2019 Weblogicx India. All rights reserved.
- * @copyright Copyright (C) 2024 J2Commerce, Inc. All rights reserved.
+ * @copyright Copyright (C) 2025 J2Commerce, Inc. All rights reserved.
  * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
  * @website https://www.j2commerce.com
  */
 
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Access\Access;
 use Joomla\CMS\Cache\Cache;
