36683f4c31b8624196beb42eabf617377eb53022
Adam Melcher <54608117+newlinewebdesign@users.noreply.github.com>
2025-02-07 17:34:59 -0600

    Downloadable Product UI Updates (#117)

--- administrator/components/com_j2store/views/product/tmpl/form_inventory.php
+++ administrator/components/com_j2store/views/product/tmpl/form_inventory.php
@@ -2,126 +2,117 @@
 /**
  * @package J2Store
  * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
+ * @copyright Copyright (c) 2024 J2Commerce . All rights reserved.
  * @license GNU GPL v3 or later
  */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-// No direct access
-defined('_JEXEC') or die;
+
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$style = '.j2store-product-inventory .input-group .form-check.form-switch .form-check-input{min-width:0;}';
+
+$wa->addInlineStyle($style, [], []);
 ?>
+
+
 <?php if(J2Store::isPro() == 1) : ?>
-<div class="j2store-product-inventory">
-	<div class="control-group form-inline">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_MANAGE_STOCK'), 'manage_stock',array('class'=>'control-label')); ?>
-		<?php echo J2Html::radio($this->form_prefix.'[manage_stock]',(isset($this->variant->manage_stock))?$this->variant->manage_stock:'',array('class'=>'controls'));?>
-	</div>
+    <div class="j2store-product-inventory">
+        <fieldset class="options-form">
+            <legend><?php echo Text::_('J2STORE_PRODUCT_TAB_INVENTORY');?></legend>
+            <div class="form-grid">
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_MANAGE_STOCK'),'manage_stock'); ?></div>
+		            <?php echo J2Html::radioBooleanList($this->form_prefix.'[manage_stock]', (isset($this->variant->manage_stock))?$this->variant->manage_stock:''); ?>
+                </div>
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_QUANTITY'), 'quantity');?></div>
+                    <div class="controls">
+			            <?php echo J2Html::text($this->form_prefix.'[sku]',(isset($this->variant->sku))?$this->variant->sku:'',array('class'=>'form-control')); ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_SKU'), 'sku'); ?></div>
+                    <div class="controls">
+	                    <?php echo J2Html::hidden($this->form_prefix.'[quantity][j2store_productquantity_id]', (isset($this->variant->j2store_productquantity_id)) ? $this->variant->j2store_productquantity_id:'',array('class'=>'input')); ?>
+	                    <?php echo J2Html::text($this->form_prefix.'[quantity][quantity]', (isset($this->variant->quantity))?$this->variant->quantity:'',array('class'=>'form-control','field_type'=>'integer')); ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_ALLOW_BACK_ORDERS'), 'allow_backorder');?></div>
+                    <div class="controls"><?php echo str_replace('<select', '<select class="form-select"', $this->allow_backorder); ?></div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_STOCK_STATUS'), 'availability'); ?></div>
+                    <div class="controls"><?php echo str_replace('<select', '<select class="form-select"', $this->availability); ?></div>
+                </div>
+                <div class="control-group mb-0">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_NOTIFY_QUANTITY'), 'notify_qty'); ?></div>
+                    <div class="controls">
+                        <div class="input-group align-items-center">
+                            <?php $attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id'=>'notify_qty' ,'disabled'=>'disabled','field_type'=>'integer','class'=>'form-control') :array('id'=>'notify_qty','field_type'=>'integer','class'=>'form-control');
+	                    echo J2Html::text($this->form_prefix.'[notify_qty]',(isset($this->variant->notify_qty)) ? $this->variant->notify_qty: '' ,$attribs); ?>
+                            <div class="form-check form-switch pt-0 qty_restriction ms-3">
+                                <input class="form-check-input storeconfig" type="checkbox" role="switch" id="config_notify_qty" value="<?php echo $this->variant->use_store_config_notify_qty;?>" name="<?php echo $this->form_prefix; ?>[use_store_config_notify_qty]">
+                                <label class="form-check-label" for="config_notify_qty"><?php echo Text::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_QUANTITY_RESTRICTION'), 'quantity_restriction'); ?></div>
+		            <?php echo J2Html::radioBooleanList($this->form_prefix.'[quantity_restriction]',(isset($this->variant->quantity_restriction))? $this->variant->quantity_restriction : '' ); ?>
+                </div>
+                <div class="control-group mb-0">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_MAX_SALE_QUANTITY'), 'max_sale_qty'); ?></div>
+                    <div class="controls">
+                        <div class="input-group align-items-center">
+	                        <?php $attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id' =>'max_sale_qty','disabled'=>'','field_type'=>'integer','class'=>'form-control') : array('id' =>'max_sale_qty','field_type'=>'integer','class'=>'form-control');
+	                        echo J2Html::text($this->form_prefix.'[max_sale_qty]',(isset($this->variant->max_sale_qty))?$this->variant->max_sale_qty:'' ,$attribs); ?>
 
-	<div class="control-group">
-		<?php
-			 echo J2Html::label(JText::_('J2STORE_PRODUCT_QUANTITY'), 'quantity',array('class'=>'control-label'));
-			//this gets saved in the productquantities table with the variant_id as the FK
-		?>
-		<?php echo J2Html::hidden($this->form_prefix.'[quantity][j2store_productquantity_id]', (isset($this->variant->j2store_productquantity_id)) ? $this->variant->j2store_productquantity_id:'',array('class'=>'input')); ?>
-		<?php echo J2Html::text($this->form_prefix.'[quantity][quantity]', (isset($this->variant->quantity))?$this->variant->quantity:'',array('class'=>'input ','field_type'=>'integer')); ?>
-	</div>
+                            <div class="form-check form-switch pt-0 qty_restriction ms-3">
+                                <input class="form-check-input storeconfig" type="checkbox" role="switch" id="store_config_max_sale_qty" value="<?php echo $this->variant->use_store_config_max_sale_qty;?>" name="<?php echo $this->form_prefix; ?>[use_store_config_max_sale_qty]" <?php echo (isset($this->variant->use_store_config_max_sale_qty) && $this->variant->use_store_config_max_sale_qty) ? 'checked' : ''; ?>>
+                                <label class="form-check-label" for="store_config_max_sale_qty"><?php echo Text::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="control-group mb-0">
+                    <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_MIN_SALE_QUANTITY'), 'min_sale_qty'); ?></div>
+                    <div class="controls">
+                        <div class="input-group align-items-center">
+                        <?php $attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id'=>'min_sale_qty' ,'disabled'=>'','field_type'=>'integer','class'=>'form-control') :array('id'=>'min_sale_qty','field_type'=>'integer','class'=>'form-control');
+                            echo J2Html::text($this->form_prefix.'[min_sale_qty]', (isset($this->variant->min_sale_qty))?$this->variant->min_sale_qty:'',$attribs); ?>
+                            <div class="form-check form-switch pt-0 qty_restriction ms-3">
+                                <input class="form-check-input storeconfig" type="checkbox" role="switch" id="store_config_min_sale_qty" value="<?php echo $this->variant->use_store_config_min_sale_qty;?>" name="<?php echo $this->form_prefix; ?>[use_store_config_min_sale_qty]" <?php echo (isset($this->variant->use_store_config_min_sale_qty) && $this->variant->use_store_config_min_sale_qty) ? 'checked' : ''; ?> />
+                                <label class="form-check-label" for="store_config_min_sale_qty"><?php echo Text::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+    </div>
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function () {
+            document.getElementById("config_notify_qty").addEventListener("click", function () {
+                this.setAttribute("value", this.checked ? 1 : 0);
+                document.getElementById("notify_qty").disabled = this.checked;
+            });
 
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_ALLOW_BACK_ORDERS'), 'allow_backorder',array('class'=>'control-label'));?>
-		<?php
-		//three select options: Do not allow, allow, but notify customer, allow
-		// Radio Btn Displaying
-			echo  $this->allow_backorder; ?>
-	</div>
+            document.getElementById("store_config_max_sale_qty").addEventListener("click", function () {
+                this.setAttribute("value", this.checked ? 1 : 0);
+                document.getElementById("max_sale_qty").disabled = this.checked;
+            });
 
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_STOCK_STATUS'), 'availability',array('class'=>'control-label')); ?>
-		<?php 	//two select options: In Stock, Out of stock ?>
-		<?php echo $this->availability; ?>
-	</div>
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_NOTIFY_QUANTITY'), 'notify_qty',array('class'=>'control-label')); ?>
-		<?php
-			$attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id'=>'notify_qty' ,'disabled'=>'disabled','field_type'=>'integer') :array('id'=>'notify_qty','field_type'=>'integer');
-			echo J2Html::text($this->form_prefix.'[notify_qty]',(isset($this->variant->notify_qty)) ? $this->variant->notify_qty: '' ,$attribs); ?>
-		<div class="qty_restriction">
-			<label for="use_store_config_notify_qty">
-			<input id="config_notify_qty"
-				   type="checkbox" value="<?php echo $this->variant->use_store_config_notify_qty;?>"
-				   name="<?php echo $this->form_prefix; ?>[use_store_config_notify_qty]"
-				   class="storeconfig"
-				<?php echo (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? 'checked' : ''; ?>
-					/>
-					<?php echo JText::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?>
-			</label>
-		</div>
-	</div>
-
-	<div class="control-group form-inline">
-				<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_QUANTITY_RESTRICTION'), 'quantity_restriction',array('class'=>'control-label')); ?>
-				<?php echo J2Html::radio($this->form_prefix.'[quantity_restriction]',(isset($this->variant->quantity_restriction))? $this->variant->quantity_restriction : '' ); ?>
-			</div>
-			<div class="control-group">
-				<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_MAX_SALE_QUANTITY'), 'max_sale_qty',array('class'=>'control-label')); ?>
-					<?php
-						$attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id' =>'max_sale_qty','disabled'=>'disabled','field_type'=>'integer') : array('id' =>'max_sale_qty','field_type'=>'integer');
-						echo J2Html::text($this->form_prefix.'[max_sale_qty]',(isset($this->variant->max_sale_qty))?$this->variant->max_sale_qty:'' ,$attribs); ?>
-					<div class="qty_restriction">
-						<label for="use_store_config_max_sale_qty">
-						<input id="store_config_max_sale_qty" type="checkbox" value="<?php echo $this->variant->use_store_config_max_sale_qty;?>"
-							   name="<?php echo $this->form_prefix; ?>[use_store_config_max_sale_qty]"
-							   class="storeconfig"
-							<?php echo (isset($this->variant->use_store_config_max_sale_qty) && $this->variant->use_store_config_max_sale_qty) ? 'checked' : ''; ?>
-						   />
-						 <?php echo JText::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?>
-						</label>
-					</div>
-				</div>
-				<div class="control-group">
-					<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_MIN_SALE_QUANTITY'), 'min_sale_qty',array('class'=>'control-label')); ?>
-					<?php
-						$attribs = (isset($this->variant->use_store_config_notify_qty) && $this->variant->use_store_config_notify_qty) ? array('id'=>'min_sale_qty' ,'disabled'=>'disabled','field_type'=>'integer') :array('id'=>'min_sale_qty','field_type'=>'integer');
-						echo J2Html::text($this->form_prefix.'[min_sale_qty]', (isset($this->variant->min_sale_qty))?$this->variant->min_sale_qty:'',$attribs); ?>
-					<div class="qty_restriction">
-						<label for="use_store_config_min_sale_qty">
-						<input id="store_config_min_sale_qty" type="checkbox" value="<?php echo $this->variant->use_store_config_min_sale_qty;?>"
-								name="<?php echo $this->form_prefix; ?>[use_store_config_min_sale_qty]"
-								class="storeconfig"
-								<?php echo (isset($this->variant->use_store_config_min_sale_qty) && $this->variant->use_store_config_min_sale_qty) ? 'checked' : ''; ?>
-								/>
-							<?php echo JText::_('J2STORE_PRODUCT_USE_STORE_CONFIGURATION'); ?>
-						</label>
-					</div>
-				</div>
-
-
-</div>
-<script type="text/javascript">
-(function($){
-	$("#config_notify_qty").click(function(){
-		$(this).attr('value',0);
-		if(this.checked == true){
-			$(this).attr('value',1);
-		}
-		$('#notify_qty').attr('disabled',this.checked);
-	});
-
-	$("#store_config_max_sale_qty").click(function(){
-			$(this).attr('value',0);
-			if(this.checked == true){
-				$(this).attr('value',1);
-			}
-			$('#max_sale_qty').attr('disabled',this.checked);
-		});
-
-		$("#store_config_min_sale_qty").click(function(){
-			$(this).attr('value',0);
-			if(this.checked == true){
-				$(this).attr('value',1);
-			}
-			$('#min_sale_qty').attr('disabled',this.checked);
-		});
-	})(j2store.jQuery);
-
-</script>
-<?php else:?>
-	<?php echo J2Html::pro(); ?>
-<?php endif;?>
+            document.getElementById("store_config_min_sale_qty").addEventListener("click", function () {
+                this.setAttribute("value", this.checked ? 1 : 0);
+                document.getElementById("min_sale_qty").disabled = this.checked;
+            });
+        });
+    </script>
+    <?php else:?>
+        <?php echo J2Html::pro(); ?>
+    <?php endif;?>
--- administrator/components/com_j2store/views/product/tmpl/form_pricing.php
+++ administrator/components/com_j2store/views/product/tmpl/form_pricing.php
@@ -2,11 +2,16 @@
 /**
  * @package J2Store
  * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
+ * @copyright Copyright (c) 2024 J2Commerce . All rights reserved.
  * @license GNU GPL v3 or later
  */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
 
-// No direct access
-defined('_JEXEC') or die;
 //pricing options
 $variant_pricing_calculator = (isset($this->variant->pricing_calculator))?$this->variant->pricing_calculator :'';
 $pricing_calculator = J2Html::select()->clearState()
@@ -15,37 +20,36 @@ $pricing_calculator = J2Html::select()->clearState()
 ->value($variant_pricing_calculator)
 ->setPlaceHolders(J2Store::product()->getPricingCalculators())
 ->getHtml();
-$base_path = rtrim(JUri::root(),'/').'/administrator';
+$base_path = rtrim(Uri::root(),'/').'/administrator';
 ?>
-<?php if (version_compare(JVERSION, '3.99.99', 'lt')): ?>
-<style>
-    .fancybox-slide--iframe .fancybox-content {
-        width: 1000px !important;
-        height: 600px !important;
-    }
-</style>
-<?php endif; ?>
+
 <div class="j2store-product-pricing">
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_REGULAR_PRICE'), 'price' ,array('class'=>'control-label')); ?>
-		<?php echo J2Html::price($this->form_prefix.'[price]',(isset($this->variant->price))? $this->variant->price:'', array('class'=>'input')); ?>
-	</div>
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_SET_ADVANCED_PRICING'), 'sale_price',array('class'=>'control-label')); ?>
-		<!-- Link to advanced pricing options. Opens as a popup. -->
-        <a data-fancybox class="btn btn-success" data-type="iframe" data-src="<?php echo $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component";?>" href="javascript:;">
-            <?php echo JText::_( "J2STORE_PRODUCT_SET_PRICES" );?>
-        </a>
-		<?php //echo J2StorePopup::popup( $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component", JText::_( "J2STORE_PRODUCT_SET_PRICES" ), array('class'=>'btn btn-success'));?>
-	</div>
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_PRICING_CALCULATOR'), 'price_calculator',array('class'=>'control-label')); ?>
-		<?php //dropdown list: pre-populate it with Standard (to start with). We will extend this at a later point of time ?>
-		<?php echo $pricing_calculator;?>
-	</div>
+    <fieldset class="options-form">
+        <legend><?php echo Text::_('J2STORE_PRODUCT_TAB_PRICE');?></legend>
+        <div class="form-grid">
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_REGULAR_PRICE'), 'price'); ?></div>
+                <div class="controls">
+	                <?php echo J2Html::price($this->form_prefix.'[price]',(isset($this->variant->price))? $this->variant->price:'', array('class'=>'form-control')); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_SET_ADVANCED_PRICING'), 'sale_price'); ?></div>
+                <div class="controls">
+                    <a data-fancybox class="btn btn-success" data-type="iframe" data-src="<?php echo $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component";?>" href="javascript:;">
+		                <?php echo Text::_('J2STORE_PRODUCT_SET_PRICES');?>
+                    </a>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_PRICING_CALCULATOR'), 'price_calculator'); ?></div>
+                <div class="controls"><?php echo str_replace('<select', '<select class="form-select"', $pricing_calculator); ?></div>
+            </div>
+        </div>
+    </fieldset>
 </div>
 
-<div class="alert alert-info">
-<h4><?php echo JText::_('J2STORE_QUICK_HELP'); ?></h4>
-<?php echo JText::_('J2STORE_PRODUCT_PRICE_HELP_TEXT'); ?>
+<div class="alert alert-info mt-3 mb-0">
+    <h4 class="alert-heading"><?php echo Text::_('J2STORE_QUICK_HELP'); ?></h4>
+    <?php echo Text::_('J2STORE_PRODUCT_PRICE_HELP_TEXT'); ?>
 </div>
