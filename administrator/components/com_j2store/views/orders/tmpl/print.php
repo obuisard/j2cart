5d6e036ee380fac59baee85ba8ec31684a460391
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-04-01 17:46:37 -0400

    Fix J2Commerce copyright

--- administrator/components/com_j2store/views/orders/tmpl/print.php
+++ administrator/components/com_j2store/views/orders/tmpl/print.php
@@ -3,7 +3,7 @@
  * @package Joomla.Administrator
  * @subpackage com_j2store
  * @copyright Copyright (c)2014-24 Ramesh Elamathi / J2Store.org
- * @copyright Copyright (c) 2024 J2Commerce . All rights reserved.
+ * @copyright Copyright (C) 2025 J2Commerce, LLC. All rights reserved.
  * @license GNU GPL v3 or later
  */
 
