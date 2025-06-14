f7943ba67d75d6840a16698032e7420d45f09018
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-06-13 22:59:51 -0400

    Obsolete code

--- administrator/components/com_j2store/sql/updates/mysql/4.1.0-2025-02-07.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `#__j2store_orderitemattributes`
-MODIFY COLUMN `orderitemattribute_price` decimal(12,4) NOT NULL /** CAN FAIL **/;
-
-ALTER TABLE `#__j2store_orderitems`
-MODIFY COLUMN `orderitem_price` decimal(12,4) NOT NULL /** CAN FAIL **/;
-
-ALTER TABLE `#__j2store_shippingrates`
-MODIFY COLUMN `shipping_rate_price` decimal(12,4) NOT NULL,
-MODIFY COLUMN `shipping_rate_handling` decimal(12,4) NOT NULL /** CAN FAIL **/;
